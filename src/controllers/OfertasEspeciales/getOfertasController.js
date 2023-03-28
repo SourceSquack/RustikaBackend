abb51f74728b59bb6fafc6a43eb75344beaba341
multichiki22 <myguelangel12@gmail.com>
2023-03-28 17:25:15 -0500

    Database connection

---
+++ src/controllers/OfertasEspeciales/getOfertasController.js
@@ -0,0 +1,18 @@
+const Offers = require("../../models/Ofertas")
+const {mongoConect} = require("../../config/db")
+
+const getOfertas = async(event, context)=>{
+  mongoConect(process.env.MONGO_URI)
+  const temp = await Offers.find({})
+    return {
+        statusCode: 200,
+        body: JSON.stringify(
+          {
+            message: temp,
+          },
+        ),
+      };
+}
+module.exports = {
+    getOfertas
+}
