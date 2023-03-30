687520bcc471ed0e0fe3d43457b48384b10bada7
PatoProgramador <spartan1171947@hotmail.com>
2023-03-29 19:03:27 -0500

    Dish post functional

--- src/controllers/PlatosYBebidas/createDish.js
+++ src/controllers/PlatosYBebidas/createDish.js
@@ -1,33 +1,57 @@
 const {mongoConect} = require("../../config/db");
 const dish = require('../../models/Platos');
+const middy = require("@middy/core");
+const jsonBodyParser = require("@middy/http-json-body-parser");
 
 const createDish = async (event) => {
-    // if(!event.body) return {
-    //     statusCode: 400,
-    //     body: JSON.stringify({"Error": "Debes pasar los campos necesarios para crear el plato/bebida"})
-    // };
-    // const { dish } = event.body;
-    mongoConect(process.env.MONGO_URI);
-
-    const newDish = {
-        name: "test",
-        img: "awdawda",
-        value: "500",
-        description: "hola soy un test",
-        category: "test",
-        subcategory: "testS"
-    }
-
-    const dishNuevo = new dish(newDish);
-
-    await dishNuevo.save();
-
-    return {
-        statusCode: 200,
-        body: JSON.stringify({
-            "message": dishNuevo
-        })
+    if(!event.body) return {
+        statusCode: 400,
+        body: JSON.stringify({"Error": "Debes pasar los campos necesarios para crear el plato/bebida"})
+    };
+    const { name, img, value, description, category, subcategory  } = event.body;
+    try {
+        //conexion con la db
+        mongoConect(process.env.MONGO_URI);
+        
+        // validacion de campos obligarios
+        const validate = {
+            name,
+            img,
+            value,
+            description,
+            category,
+            subcategory
+        };
+        for(const key in validate) {
+            const element = validate[key];
+            if (!element) {
+                return {
+                    statusCode: 400,
+                    body: JSON.stringify({
+                        "message": `el campo ${key} no puede estar vacio`
+                    })
+                }
+            };
+        };
+        // creacion de la nueva instancia en la db
+        const dishNuevo = new dish(validate);
+        await dishNuevo.save();
+    
+        return {
+            statusCode: 200,
+            body: JSON.stringify({
+                "message": "El plato/bebida se ha creado correctamente"
+            })
+        };
+    } catch (error) {
+        return {
+            statusCode: 500,
+            body: JSON.stringify({"Error": error})
+        };
     }
 };
 
-module.exports = { createDish }
+module.exports = { 
+    createDish: middy(createDish)
+        .use(jsonBodyParser())
+};
