f4aed88dfb77918d1bb777b0fb1a5ddfa89412db
PatoProgramador <spartan1171947@hotmail.com>
2023-03-29 22:17:39 -0500

    Delete dish working

--- src/controllers/PlatosYBebidas/deleteDish.js
+++ src/controllers/PlatosYBebidas/deleteDish.js
@@ -1,9 +1,30 @@
+const { mongoConect } = require("../../config/db");
+const dish = require('../../models/Platos');
+
 const deleteDish = async (event) => {
-    const id = event.pathParameters
-    return {
-        statusCode: 200,
-        body: JSON.stringify(id)
+    const {id} = event.pathParameters
+    try {
+        mongoConect(process.env.MONGO_URI);
+        let dishdelete = await dish.deleteOne({_id: id});
+        if (dishdelete.deletedCount === 1) {
+            return {
+                statusCode: 200,
+                body: JSON.stringify({"message": "El plato/bebida se eliminó correctamente"})
+            }
+        } else {
+            return {
+                statusCode: 400,
+                body: JSON.stringify({"error": "Ocurrió un error eliminando el plato/bebida"})
+            }
+        }
+
+    } catch (error) {
+        return {
+            statusCode: 400,
+            body: JSON.stringify({"error": error})
+        }
     }
+
 };
 
 module.exports = {deleteDish}
