46071d12e6bc4ffd85bab9d253f779a487e2934b
PatoProgramador <spartan1171947@hotmail.com>
2023-03-29 20:03:01 -0500

    basic get dishes with paginate, missing filters

---
+++ src/controllers/PlatosYBebidas/getDishes.js
@@ -0,0 +1,41 @@
+const { mongoConect } = require("../../config/db");
+const dish = require('../../models/Platos');
+const middy = require("@middy/core");
+const jsonBodyParser = require("@middy/http-json-body-parser");
+
+const getDishes = async (event) => {
+    let options = {
+        page: 1,
+        limit: 5
+    };
+    if (event.queryStringParameters) {
+        const { page , limit } = event.queryStringParameters;
+        options = {
+            page,
+            limit
+        }
+    };
+
+    try {
+        mongoConect(process.env.MONGO_URI);
+        let filteredDishes;
+        filteredDishes = await dish.paginate({}, options);
+        // filteredDishes = filteredDishes.docs
+        return {
+            statusCode: 200,
+            body: JSON.stringify({
+                filteredDishes
+            })
+        }
+    } catch (error) {
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ "Error": error })
+        };
+    }
+};
+
+module.exports = {
+    getDishes: middy(getDishes)
+        .use(jsonBodyParser())
+};
