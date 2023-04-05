4600e2a852b970551dd847ac7b9c5427a05f30d4
PatoProgramador <spartan1171947@hotmail.com>
2023-04-04 22:50:15 -0500

    Upload images service ok and tested

--- src/controllers/uploadTest.js
+++ src/controllers/uploadTest.js
@@ -2,34 +2,7 @@ const middy = require("@middy/core");
 const jsonBodyParser = require("@middy/http-json-body-parser");
 const urlencodeParser = require("@middy/http-urlencode-body-parser");
 const multiparDataParser = require("@middy/http-multipart-body-parser");
-const AWS = require("aws-sdk");
-const s3 = new AWS.S3({
-  accessKeyId: process.env.NEW_AWS_ACCESS_KEY,
-  secretAccessKey: process.env.NEW_AWS_SECRET_ACCESS_KEY,
-  Bucket: process.env.BUCKET_NAME,
-  ACL:'public-read'
-});
-
-const uploadFile = (filename, data, contentType) => {
-  console.log('Subiendo...');
-  return new Promise((resolve, reject) => {
-    const params = {
-      Bucket: process.env.BUCKET_NAME,
-      Key: filename,
-      Body: Buffer.from(data, 'base64'),
-      ContentEncoding: 'base64',
-      ContentType: contentType
-    }
-    s3.upload(params, function(s3Err, data) {
-        if(s3Err) {
-          console.log(s3Err);
-          reject(s3Err)
-        }
-        console.log('Se subio correctamente');
-        resolve(data)
-    });
-  });
-}
+const { uploadFile } = require('../services/uploadImage');
 
 const postTestImage = async (event, context) => {
   const request = event.body;
