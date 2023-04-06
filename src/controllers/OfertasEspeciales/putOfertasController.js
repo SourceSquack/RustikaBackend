dc69943adc3b53a6655fae8718ccccf6094d71fd
multichiki22 <myguelangel12@gmail.com>
2023-04-05 19:39:35 -0500

    Added image file support

--- src/controllers/OfertasEspeciales/putOfertasController.js
+++ src/controllers/OfertasEspeciales/putOfertasController.js
@@ -4,6 +4,7 @@ const middy = require("@middy/core");
 const urlencodeParser = require("@middy/http-urlencode-body-parser");
 const jsonBodyParser = require("@middy/http-json-body-parser");
 const multiparDataParser = require("@middy/http-multipart-body-parser");
+const { uploadFile } = require("../../services/uploadImage");
 
 Date.prototype.isValid = function () {
   return this.getTime() === this.getTime();
@@ -23,12 +24,31 @@ const putOfertas = async (event, context) => {
     };
   try {
     const offerToUpdate = await Offers.findById(id).exec();
-    if (offerToUpdate == null)return {
-      statusCode: 400,
-      body: JSON.stringify({ Error: "No se encontro una oferta con ese ID" }),
-    };
+    if (offerToUpdate == null)
+      return {
+        statusCode: 400,
+        body: JSON.stringify({ Error: "No se encontro una oferta con ese ID" }),
+      };
     if (image) {
-      offerToUpdate.image = image;
+      if (
+        image.mimetype !== "image/png" &&
+        image.mimetype !== "image/jpeg" &&
+        image.mimetype !== "image/webp"
+      )
+        return {
+          statusCode: 400,
+          body: JSON.stringify({
+            Error:
+              "Formato de archivo no sorportado: Formato enviado: " +
+              image.mimetype,
+          }),
+        };
+      const uploadedImage = await uploadFile(
+        image.filename,
+        image.content,
+        "image/jpeg"
+      );
+      offerToUpdate.image = uploadedImage.Location;
     }
     if (initialDate && finalDate) {
       const dateObjectInitialDate = new Date(initialDate);
@@ -99,7 +119,7 @@ const putOfertas = async (event, context) => {
     await offerToUpdate.save();
     return {
       statusCode: 200,
-      body: JSON.stringify({ message: "Actualizado"}),
+      body: JSON.stringify({ message: "Actualizado" }),
     };
   } catch (error) {
     if (error.name === "CastError")
