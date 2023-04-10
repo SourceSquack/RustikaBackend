b68c6808c569439001d0d639ae8b55f1e46343ab
PatoProgramador <spartan1171947@hotmail.com>
2023-04-09 23:35:43 -0500

    Filters by name and category

--- src/controllers/Platos/getDishes.js
+++ src/controllers/Platos/getDishes.js
@@ -2,38 +2,37 @@ const { mongoConect } = require("../../config/db");
 const dish = require('../../models/Platos');
 
 const getDishes = async (event) => {
+    // variables de paginado y filtro, respectivamente
     let options = {
         page: 1,
         limit: 5
     };
+
+    let params = {
+        name: undefined,
+        category: undefined
+    };
     // validaciones de params
     if (event.queryStringParameters) {
-        let { page , limit } = event.queryStringParameters;
-
-        if(page && limit) {
-            page = parseInt(page)
-            limit = parseInt(limit)
-            options = {
-                page,
-                limit
+        let paramsEvent = event.queryStringParameters;
+        // opciones del paginado
+        for(key in options) {
+            if(paramsEvent[key]) {
+                options = {
+                    ...options,
+                    [key]: parseInt(paramsEvent[key]) 
+                }
             }
         }
-
-        if(page) {
-            page = parseInt(page)
-            options = {
-                ...options,
-                page
+        // busqueda de filtros pasados por query
+        for(key in params) {
+            if(paramsEvent[key]) {
+                params = {
+                    ...params,
+                    [key]: paramsEvent[key]
+                }
             }
-        }
-
-        if(limit) {
-            limit = parseInt(limit)
-            options = {
-                ...options,
-                limit
-            }
-        }
+        };
     };
 
     try {
@@ -41,16 +40,35 @@ const getDishes = async (event) => {
         mongoConect(process.env.MONGO_URI);
 
         let filteredDishes;
-        // Filtro por nombre
-        if(event.queryStringParameters.name) {
+        // Busqueda de docs por filtros
+        // Filtro por nombre y categoria
+        if(params.name && params.category) {
             const name = event.queryStringParameters.name;
-            const regex = new RegExp(name, "i"),
+            const regex = new RegExp(name, "i");
+            filteredDishes = await dish.paginate({ name: { $regex: regex}, category: params.category}, options);
+            return {
+                statusCode: 200,
+                body: JSON.stringify(filteredDishes)
+            }
+        };
+        // Filtro por nombre
+        if(params.name) {
+            const name = event.queryStringParameters.name;
+            const regex = new RegExp(name, "i");
             filteredDishes = await dish.paginate({ name: { $regex: regex}}, options);
             return {
                 statusCode: 200,
                 body: JSON.stringify(filteredDishes)
             }
-        }
+        };
+        // Filtro por categoria
+        if (params.category) {
+            filteredDishes = await dish.paginate({category: params.category},options);
+            return {
+                statusCode: 200,
+                body: JSON.stringify(filteredDishes)
+            }
+        };
         // Todos los docs por defecto
         filteredDishes = await dish.paginate({}, options);
         return {
