c04c40129433368ca345d0f636d8e99b3b254832
multichiki22 <myguelangel12@gmail.com>
2023-04-12 00:16:39 -0500

    Added timeBased expired filtering

---
+++ src/controllers/TimeBasedControllers/expiredDates.js
@@ -0,0 +1,24 @@
+const { mongoConect } = require("../../config/db");
+const Offers = require("../../models/Ofertas");
+const discounts = require("../../models/Descuentos");
+const booking = require("../../models/Reservas");
+mongoConect(process.env.MONGO_URI);
+
+const filterExpiredDates = async (event) => {
+  const toDay = new Date();
+  try {
+    console.log("Filter dates execution");
+    await Offers.deleteMany({ finalDate: { $lt: toDay } });
+    await discounts.deleteMany({ finalDate: { $lt: toDay } });
+    await booking.updateMany(
+      { finalDateTime: { $lt: toDay } },
+      { active: false }
+    );
+    console.log("Filter dates completion");
+  } catch (error) {
+    console.log(error);
+  }
+};
+module.exports = {
+  filterExpiredDates,
+};
