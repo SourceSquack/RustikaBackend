8afc759214d5ecdc37fb983bfda1fac8579d3861
multichiki22 <myguelangel12@gmail.com>
2023-04-13 03:48:05 -0500

    fix models & activation/deactivation events

--- src/models/Platos.js
+++ src/models/Platos.js
@@ -1,40 +1,49 @@
-const mongoose = require('mongoose');
-const mongoosePaginate = require('mongoose-paginate');
+const mongoose = require("mongoose");
+const mongoosePaginate = require("mongoose-paginate");
 
 const dishSchema = new mongoose.Schema(
-    {
-        name: {
-            type: String,
-            required: true
-        },
-        img: {
-            type: String,
-            required: true
-        },
-        units: {
-            type: Number
-        },
-        value: {
-            type: Number,
-            required: true
-        },
-        description: {
-            type: String,
-        },
-        category: {
-            type: String,
-            required: true
-        },
-        discount: {
-            type: Boolean,
-            default: false
-        }
-    }, {
-        versionKey: false,
-        timestamps: true,
-    }
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    img: {
+      type: String,
+      required: true,
+    },
+    units: {
+      type: Number,
+    },
+    value: {
+      type: Number,
+      required: true,
+    },
+    description: {
+      type: String,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    discount: {
+      type: Boolean,
+      default: false,
+    },
+    percentage: {
+      type: Number,
+      required: false,
+    },
+    newValue: {
+      type: Number,
+      required: false,
+    },
+  },
+  {
+    versionKey: false,
+    timestamps: true,
+  }
 );
 
 dishSchema.plugin(mongoosePaginate);
 
-module.exports = mongoose.model('dish', dishSchema);
+module.exports = mongoose.model("dish", dishSchema);
